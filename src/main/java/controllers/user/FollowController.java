61fd28f39516f530824b615052befb7f8671a277
wopqw <wopqw@yandex.ru>
2016-12-11 13:33:45 +0300

    todo done

--- src/main/java/controllers/user/FollowController.java
+++ src/main/java/controllers/user/FollowController.java
@@ -19,7 +19,6 @@ import java.util.Optional;
 @Slf4j
 @WebServlet(urlPatterns = "/s/follow")
 public class FollowController extends BaseServlet {
-    // TODO: 13.11.16 Обработка случая при неавторизированном пользователе на странице
     private HttpSession httpSession;
 
     @Override
